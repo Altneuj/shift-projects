826a3cb903be80ce2363368fa632473be7c43598
Altneuj <34726282+Altneuj@users.noreply.github.com>
2018-05-01 10:44:28 -0400

    finished JSON practice

---
+++ json-design/main.js
@@ -0,0 +1,91 @@
+"animals": [
+{
+  "name": "humans",
+  "weight": {
+    "kilograms": 70,
+    "tons": 0.077
+  },
+  "habitat": "Earth",
+  "endangered": false,
+  "food": [
+    "pizza",
+    "hamburger"
+  ],
+  "predator": [
+    "shark",
+    "bear"
+  ],
+  "delicious": "??"
+},
+{
+    "name": "cattle",
+    "weight": {
+      "kilograms": 550,
+      "tons": 0.606
+    },
+    "habitat": "farm",
+    "endangered": false,
+    "food": [
+      "grass",
+      "corn"
+    ],
+    "predator": [
+      "humans",
+      "wolf"
+    ],
+    "delicious": 5
+  },
+  {
+    "name": "elephant",
+    "weight": {
+      "kilograms": 2000,
+      "tons": 2.20
+    },
+    "habitat": "Africa",
+    "endangered": true,
+    "food": [
+      "grass",
+      "fruits"
+    ],
+    "predator": [
+      "lion",
+      "tigers"
+    ],
+    "delicious": 2
+  },
+  {
+    "name": "Grey wolf",
+    "weight": {
+      "kilograms": 38.5,
+      "tons": 0.42
+    },
+    "habitat": "North America",
+    "endangered": false,
+    "food": [
+      "squirrel",
+      "humans"
+    ],
+    "predator": [
+      "humans"
+    ],
+    "delicious": 2
+  },
+  {
+    "name": "squirrel",
+    "weight": {
+      "kilograms": 5,
+      "tons": .0055
+    },
+    "habitat": "Earth",
+    "endangered": false,
+    "food": [
+      "grass",
+      "nuts"
+    ],
+    "predator": [
+      "snakes",
+      "eagle"
+    ],
+    "delicious": 3
+  }
+]
